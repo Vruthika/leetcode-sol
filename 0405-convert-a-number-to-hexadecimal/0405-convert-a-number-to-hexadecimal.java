8015321f4c29d8cfdb9a67f12a3b4be5c28fc74b
Vruthika L S <126603138+Vruthika@users.noreply.github.com>
2024-09-23 10:06:57 +0530

    Time: 0 ms (100%), Space: 41.2 MB (22.97%) - LeetHub

--- 0405-convert-a-number-to-hexadecimal/0405-convert-a-number-to-hexadecimal.java
+++ 0405-convert-a-number-to-hexadecimal/0405-convert-a-number-to-hexadecimal.java
@@ -8,9 +8,15 @@ class Solution {
         
         while(num!=0)
         {
-            int rem=num & 15;
+            int rem=num & 15;   //equivalent to num%16 and  efficiently extracts the last hexadecimal digit
             res.append(d[rem]);
-            num>>>=4;
+            num>>>=4;  //equivalent to num/16 (unsigned right shift operation)
+
+            //This operation treats num as an unsigned 32-bit integer,
+            // which is crucial for correctly handling negative numbers 
+            //in the context of bitwise operations. In Java, negative numbers are
+            // represented in two's complement format, so >>> ensures that the shift is unsigned,
+            // avoiding issues with sign extension
         }
 
         return res.reverse().toString();
