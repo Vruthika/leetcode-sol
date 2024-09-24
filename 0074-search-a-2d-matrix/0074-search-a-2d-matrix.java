7d40efb5319ce1fdffdf79e69d0fa090e98a94a6
Vruthika L S <126603138+Vruthika@users.noreply.github.com>
2024-09-24 13:44:01 +0530

    Time: 0 ms (100%), Space: 42.1 MB (38.38%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,4 +1,26 @@
 class Solution {
+    public boolean searchMatrix(int[][] arr, int t) {
+        int row=arr.length;
+        int col=arr[0].length;
+        int left=0;
+        int right=row*col-1;
+        while(left<=right)
+        {
+            int mid=left+(right-left)/2;
+            int midelem=arr[mid/col][mid%col];
+            if(midelem==t)
+                return true;
+            if(midelem>t)
+                right=mid-1;
+            else
+                left=mid+1;
+        }
+        return false;
+    }
+}
+
+/*
+class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         for(int i=0;i<matrix.length;i++)
         {
@@ -12,4 +34,4 @@ class Solution {
         }
         return false;
     }
-}
+}*/
