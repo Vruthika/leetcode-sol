47c43a36d8d091c5a4a1297a73f6d83466925aa8
Vruthika L S <126603138+Vruthika@users.noreply.github.com>
2024-09-24 13:47:37 +0530

    Time: 0 ms (100%), Space: 45.1 MB (72.84%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -9,3 +9,24 @@ class Solution {
         return sum-tot;
     }
 }
+
+/*class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        if(nums[0]!=0)
+        return 0;
+        boolean fl=true;
+        int r=0;
+        for(int i=0;i<nums.length-1;i++)
+        {
+            if(nums[i+1]-nums[i]!=1)
+            {
+                fl=false;
+                r=i+1;
+            }
+        }
+        if(fl==true)
+        r=nums.length;
+        return r;
+    }
+}*/
